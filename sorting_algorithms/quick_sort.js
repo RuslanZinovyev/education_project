2849d20e0d00f2ca16a189115812d7f3a6f9fbaf
rzinovyev <ruslan.zinovyev@hotmail.com>
2021-08-06 19:17:04 -0400

    changed space complexity

--- sorting_algorithms/quick_sort.js
+++ sorting_algorithms/quick_sort.js
@@ -5,7 +5,7 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 /**
  * Simple implementation of Quick sort algorithm.
  * O(n log n) time complexity
- * O(n) space complexity
+ * O(log n) space complexity
  * @param {*} array
  */
 function quickSort(array, begin, end) {
